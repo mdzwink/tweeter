234dacfc9d7e12d7aca434292375c7daef58bc9a
mdzwink <mdzwink@gmail.com>
2022-05-26 09:36:08 -0400

    implementing createTweetElement fn and renderTweets fn

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,24 +3,90 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const tweetData = {
+
+
+const data = [
+  {
     "user": {
       "name": "Newton",
       "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
+      "handle": "@SirIsaac"
+    },
     "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1653356348716
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1653442748716
   }
+]
 
-const $tweet = createTweetElement(tweetData)
+// const timeStamp = function(created_at) {
+//   const currentTime = new Date(0);
+//   console.log(currentTime)
+//   const 
+//   const formattedTime = 
+// }
 
-console.log($tweet);
-
-$('#tweets-container').append($tweet);
 
 $(document).ready(function() {
-  const $tweet = $(`<article class="tweet">Hello world</article>`);
+  console.log('ready from client.js, tweet gen');
+
+  const $submit = $('tweet-button-div');
+
+  $submit.on('submit', function(){
+    console.log('submission attempted')
+
+  })
+
+
+
+
+
+  const renderTweets = function(tweets) {
+    for (let aTweet of tweets) {
+      console.log('found a tweet')
+      const $tweet = createTweetElement(aTweet)
+      $('#tweets-container').append($tweet);
+    }  
+  }
+  const createTweetElement = function($tweet) {
+  const twtUsr = $tweet.user;
+  const created_at = $tweet['created_at'];
+  let timeStamp = created_at
+  const tweetCompiler = $(`
+      <article>
+        <header>
+          <div class="user-name">
+          <div><img src="${twtUsr.avatars}"></div>
+          <div>${twtUsr.name}</div>
+          </div>
+          <div>${twtUsr.handle}</div>
+        </header>
+        <p>${$tweet.content.text}</p>
+        <footer>
+        <div>${timeStamp}</div>
+        <div class="reactions">
+          <div class="flag"><i class="fas fa-flag"></i></div>
+          <div class="retweet" ><i class="fa fa-retweet" aria-hidden="true"></i></div>
+          <div class="like"><i class="fa-solid fa-heart"></i></div>
+        </div>
+      </footer>
+    </article>`);
+  
+  return tweetCompiler;
+  } 
+
+renderTweets(data);
+
+
 })
--- public/scripts/posted-tweet.js
+++ public/scripts/posted-tweet.js
@@ -1,6 +1,35 @@
 //box shadow on mouse over
 //buttons highlight on mouse over
 
+
+// const mouseOverFlag = function(event) {
+//   console.log('mouse is over')
+//   this.style.color = 'red';
+// }
+// const mouseLeaveFlag = function(event) {
+//   console.log('mouse has left')
+//   this.style.color = 'inherit';
+// }
+// const mouseOverRetweet = function(event) {
+//   console.log('mouse is over')
+//   this.style.color = 'red';
+// }
+// const mouseLeaveRetweet = function(event) {
+//   console.log('mouse has left')
+//   this.style.color = 'inherit';
+// }
+// const mouseOverLike = function(event) {
+//   console.log('mouse is over')
+//   this.style.color = 'red';
+// }
+// const mouseLeaveLike = function(event) {
+//   console.log('mouse has left')
+//   this.style.color = 'inherit';
+// }
+
+
+
+
 const mouseOver = function(event) {
   console.log('mouse is over')
   this.style['box-shadow'] = '1px 1px 9px 1px #c9beb5';
@@ -9,42 +38,23 @@ const mouseLeave = function(event) {
   console.log('mouse has left')
   this.style['box-shadow'] = 'none';
 }
-const mouseOverFlag = function(event) {
-  console.log('mouse is over')
-  this.style.color = 'red';
-}
-const mouseLeaveFlag = function(event) {
-  console.log('mouse has left')
-  this.style.color = 'inherit';
-}
-const mouseOverRetweet = function(event) {
-  console.log('mouse is over')
-  this.style.color = 'red';
-}
-const mouseLeaveRetweet = function(event) {
-  console.log('mouse has left')
-  this.style.color = 'inherit';
-}
-const mouseOverLike = function(event) {
-  console.log('mouse is over')
-  this.style.color = 'red';
-}
-const mouseLeaveLike = function(event) {
-  console.log('mouse has left')
-  this.style.color = 'inherit';
-}
+
+
 $(document).ready(function() {
   const $postCard = document.querySelector('article');
   $postCard.addEventListener('mouseover', mouseOver)
   $postCard.addEventListener('mouseleave', mouseLeave)
-  const $flag = document.querySelector('flag');
-  const $retweet = document.querySelector('retweet');
-  const $like = document.querySelector('like');
-  $flag.addEventListener('mouseover', mouseOverFlag)
-  $flag.addEventListener('mouseleave', mouseLeaveFlag)
-  $retweet.addEventListener('mouseover', mouseOverRetweet)
-  $retweet.addEventListener('mouseleave', mouseLeaveRetweet)
-  $like.addEventListener('mouseover', mouseOverLike)
-  $like.addEventListener('mouseleave', mouseLeaveLike)
-
+    
 })
+
+
+
+// const $flag = document.querySelector('flag');
+// const $retweet = document.querySelector('retweet');
+// const $like = document.querySelector('like');
+// $flag.addEventListener('mouseover', mouseOverFlag)
+// $flag.addEventListener('mouseleave', mouseLeaveFlag)
+// $retweet.addEventListener('mouseover', mouseOverRetweet)
+// $retweet.addEventListener('mouseleave', mouseLeaveRetweet)
+// $like.addEventListener('mouseover', mouseOverLike)
+// $like.addEventListener('mouseleave', mouseLeaveLike)
